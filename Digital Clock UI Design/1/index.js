bfc9012a488d32b12849b539cf0bc042efb23fa1
GlassK <glassk0724@gmail.com>
2021-09-03 11:21:36 +0900

    feat: Digital Clock UI Design

---
+++ Digital Clock UI Design/1/index.js
@@ -0,0 +1,15 @@
+function clock() {
+  const hours = document.getElementById('hour');
+  const minutes = document.getElementById('minutes');
+  const seconds = document.getElementById('seconds');
+
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+}
+
+let interval = setInterval(clock, 1000);
