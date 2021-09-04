973c0d27856218bb96768c37fc80e9c681508856
GlassK <glassk0724@gmail.com>
2021-09-04 09:50:46 +0900

    feat: Popup with Blurred Background

---
+++ Popup with Blurred Background/index.js
@@ -0,0 +1,6 @@
+function toggle() {
+  const blur = document.getElementById('blur');
+  const popup = document.getElementById('popup');
+  blur.classList.toggle('active');
+  popup.classList.toggle('active');
+}
