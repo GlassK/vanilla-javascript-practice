68376f5ca7f2d42f88544778672de534e056322a
GlassK <glassk0724@gmail.com>
2021-09-09 16:40:43 +0900

    feat: Responsive Website From Scratch

---
+++ Responsive Website From Scratch/index.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const slideshow = document.getElementById('slideshow');
+const slides = slideshow.getElementsByTagName('img');
+let index = 0;
+
+function nextSlide() {
+  slides[index].classList.remove('active');
+  index = (index + 1) % slides.length;
+  slides[index].classList.add('active');
+}
+
+function prevSlide() {
+  slides[index].classList.remove('active');
+  index = (index - 1 + slides.length) % slides.length;
+  slides[index].classList.add('active');
+}
+
+const slideshowText = document.getElementById('slideshowText');
+const slidesText = slideshowText.getElementsByTagName('div');
+let i = 0;
+
+function nextSlideText() {
+  slidesText[i].classList.remove('active');
+  i = (i + 1) % slidesText.length;
+  slidesText[i].classList.add('active');
+}
+
+function prevSlideText() {
+  slidesText[i].classList.remove('active');
+  i = (i - 1 + slidesText.length) % slidesText.length;
+  slidesText[i].classList.add('active');
+}
+
+function menuToggle() {
+  const nav = document.getElementById('navbar');
+  nav.classList.toggle('active');
+}
